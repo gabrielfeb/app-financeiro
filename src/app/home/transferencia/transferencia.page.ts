dd4513e5ae31f54656bbfe7feb4450e992e287c4
Viniie1 <viniciusfgdm11@gmail.com>
2021-06-07 21:20:11 -0300

    Logica de BackEnd para armazenamento no BD

--- src/app/home/transferencia/transferencia.page.ts
+++ src/app/home/transferencia/transferencia.page.ts
@@ -1,34 +1,61 @@
-import { Component, OnInit } from '@angular/core';
-import { AlertController, NavController } from '@ionic/angular';
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { Storage } from '@ionic/storage-angular'
+
 
 @Component({
   selector: 'transferencia',
   templateUrl: 'transferencia.page.html',
   styleUrls: ['transferencia.page.scss']
 })
-export class TransferenciaPage implements OnInit {
+export class TransferenciaPage {
 
-public cash = 10000;
-public transfer = 0;
-public conta = ''
+  
+  public cash = 0.00;
+  public transfer = 0
+  public n 
+  public c
+  public a 
+  public b
+  public d 
+  public e 
+  public g
 
-public decrement (){
-  this.cash -=this.transfer;
-}
 
 
-  constructor(private alertCtrl: AlertController) { }
-
-  ngOnInit() {
+  constructor(public storage: Storage) {
+    this.storage.create();
   }
-  async showAlerta(){
-    const alerta = await this.alertCtrl.create({
-      header: 'TRANSFERÊNCIA REALIZADA COM SUCESSO',
-      message:'SEU DINHEIRO SERÁ ENVIADO EM ATÉ 24H',
-      buttons:['OK']
-    });
 
-    alerta.present();
+  nome(nome: String){
+    this.n = nome;
+  }
+
+  conta(conta: String){
+    this.c = conta;
+  }
+
+
+  salvar(){
+    this.a = this.n
+    this.b = this.c
+    localStorage.setItem(this.a, this.a)
+    localStorage.setItem(this.b, this.b)
+  }
+  pegar(){
+    this.d = 'Nome: ' + localStorage.getItem(this.a)
+    this.e = 'Conta: ' + localStorage.getItem(this.b)
+    this.g = "Última conta usada"
+    console.log( this.d)
+    console.log(this.e)
+  }
+
+  
+  public decrement (variable, variable2, variable3): void{
+  this.cash -=this.transfer;
+  variable.value = null
+  variable2.value = null
+  variable3.value = null
   }
 }
 
